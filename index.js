cc6a170b4a2d89783313661ddb6617e0321c5e30
ItVal <valentinhonvj1@gmail.com>
2022-12-30 14:40:17 +0100

    register router

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express, { urlencoded } from 'express';
 const app = express();
 
+//middlewares
+app.use(urlencoded({ extended: false}));
+app.use(express.json());
 
+
+//routes
+app.post('/register', (req, res) => {
+    
+})
 
 
 app.listen(2030);
